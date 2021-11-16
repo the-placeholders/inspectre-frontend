0c8e70c6c5d46caf639ccb08ecc80ec1acc2a85b
Brannon Starnes <brannlee808@gmail.com>
2021-11-16 10:25:47 -0800

    title glitch applied

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 import "../css/title.css";
 
 class Title extends React.Component {
   render() {
     return (
-      <Container>
-        <h1>{`In\{spectre\}`}</h1>
-      </Container>
+      <Row>
+        <Col md={4}>
+          <h1 class="glitch">
+            <span aria-hidden="true">{`In\{spectre\}`}</span>
+            {`In\{spectre\}`}
+            <span aria-hidden="true">{`In\{spectre\}`}</span>
+          </h1>
+        </Col>
+      </Row>
     );
   }
 }
